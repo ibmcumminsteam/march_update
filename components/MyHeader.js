8c2bd538d0af834a2d848b7e29193c12349acd4f
ibmcumminsteam <56838139+ibmcumminsteam@users.noreply.github.com>
2020-03-01 21:11:29 +0530

    Update MyHeader.js

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -10,13 +10,24 @@ import {
   // Button,
   StyleSheet,
   Image,
+  BackHandler,
   NativeAppEventEmitter,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import {Header, SearchBar} from 'react-native-elements';
 
+
+
 class MyHeader extends React.Component{
+
+    exitApp=()=>{
+
+        fetch('http://<Insert your IP address>/ibm/exitApp.php');
+        // alert('exit app??');
+        BackHandler.exitApp();
+
+    }
     render(){
   
         return (
@@ -26,7 +37,7 @@ class MyHeader extends React.Component{
                 barStyle="light-content" // or directly
                 leftComponent={{ icon: 'menu', color: '#fff' }}
                 centerComponent={{ text: 'IBM Cloud Pak System', style: { color: '#fff'} }}
-                rightComponent={{ icon: 'home', color: '#fff' }}
+                rightComponent={{ icon: 'home', color: '#fff', onPress: () => this.exitApp()}}
                 containerStyle={{
                 backgroundColor: '#14152b',
                 justifyContent: 'space-around',
